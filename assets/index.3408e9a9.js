47ec64322edab20849ad050d30337dfbdbf8b498
tamhgdc <108126724+tamhgdc@users.noreply.github.com>
2022-07-30 06:35:53 +0700

    Add files via upload

---
+++ assets/index.3408e9a9.js
@@ -0,0 +1,1 @@
+const s=r=>{let e=parseInt(r/1e3/60),t=parseInt(r/1e3%60);return e<10&&(e="0"+e),t<10&&(t="0"+t),e+":"+t};export{s as m};
---
+++ assets/newmv.147c3ca2.js
@@ -0,0 +1,1 @@
+import o from"./mv.e9646c1c.js";import{d as r,f as t,I as e}from"./vendor.6e3b32fe.js";/* empty css                         */import"./index.cb987c5e.js";const f=r({setup(p){return(a,c)=>(t(),e(o))}});export{f as default};
